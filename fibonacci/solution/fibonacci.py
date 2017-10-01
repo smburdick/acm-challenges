72973a0e105ee6ea33819f08ccb825d9863935b9
Sam Burdick <smburdick@pugetsound.edu>
2017-10-01 01:06:22 -0700

    updated comments and examples

--- fibonacci/solution/fibonacci.py
+++ fibonacci/solution/fibonacci.py
@@ -4,15 +4,15 @@ def fib(n):
       return n
   return fib(n-1) + fib(n-2)
 
-# Better solution if we know the upper bound of n
+# Better solution, if we know the upper bound of n
 upperBound = 1000
-precomputedFibs = [1,1]
-for k in range(2, upperBound):
+precomputedFibs = [0,1]
+for k in range(2, upperBound): # memoize the first 1000 values
     precomputedFibs.append(precomputedFibs[k-1] + precomputedFibs[k-2])
 
 def better_fib(n):
-    return precomputedFibs[n - 1] #
+    return precomputedFibs[n - 1] # switch from 1-based indexing to 0-based
 
-print better_fib(5)  # 8
-print better_fib(10) # 89
-print better_fib(37) # 39088169
+print better_fib(7)  # 8
+print better_fib(12) # 89
+print better_fib(39) # 39088169
